3634fe3ed41e088955e14e5adf920b8b93e6ecf3
abelalvarz <abel.alvarez.dev@outlook.com>
2024-09-27 12:53:15 -0600

    rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import NavbarComponent from './components/NavbarComponent';
 import { Route, Routes } from 'react-router-dom';
-import SongList from './pages/SongList/SongList';
-import Repertoire from './pages/Repertoire/Repertoire';
-import Logo from './components/Logo';
-import HomePage from './pages/Home/HomePage';
 
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "/node_modules/primeflex/primeflex.css";
 import 'primeicons/primeicons.css';
+import HomePage from './pages/home/HomePage';
+import SongList from './pages/songlists/SongList';
+import Repertoire from './pages/repertoire/Repertoire';
+import Logo from './components/Logo';
 
 function App() {
   return (
