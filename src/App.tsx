70f6a8f75b2c2d5c78980e018f680ab77519e442
abelalvarz <abel.alvarez.dev@outlook.com>
2024-09-27 12:40:21 -0600

    resolve deploy conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import NavbarComponent from './components/NavbarComponent'
 import { Route, Routes } from 'react-router-dom'
-
-import "primereact/resources/themes/lara-light-cyan/theme.css";
-import "/node_modules/primeflex/primeflex.css"
-import 'primeicons/primeicons.css';
-
 import SongList from './pages/SongList/SongList';
 import Repertoire from './pages/Repertoire/Repertoire';
-import Home from './pages/Home/Home';
 import Logo from './components/Logo';
+import HomePage from './pages/Home/Home';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "/node_modules/primeflex/primeflex.css";
+import 'primeicons/primeicons.css';
 
 function App() {
   return (
@@ -16,7 +14,7 @@ function App() {
       <NavbarComponent />
       <Logo/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/lists" element={<SongList />} />
         <Route path="/repertoire" element={<Repertoire />} />
       </Routes>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import CurrentList from "./components/CurrentList";
 
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div style={{ maxWidth: '768px' }} className="page-container">
             <CurrentList />
